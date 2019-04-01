f3a8794c9066efb45060f194513fa3ae093ec0b6
Alexander Butler <butlera@aldenv167.allegheny.edu>
2019-04-01 13:59:17 -0400

    change image names

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
 import Footer from './Footer'
-import avatar from '../assets/images/propic1.jpg'
+import avatar from '../assets/images/avatar.jpg'
 
 class Header extends React.Component {
     render() {
         return (
             <header id="header">
                 <div className="inner">
-                    <a href="#" className="image avatar"><img src={propic1} alt="" /></a>
+                    <a href="#" className="image avatar"><img src={avatar} alt="" /></a>
                     <h1><strong>Tubby Black</strong>, listen...<br />
                     you can almost hear it.<br /></h1>
                 </div>
