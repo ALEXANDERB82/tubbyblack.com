39bdfa03c1b16cddf022e45c0b60704b617c838f
unknown <butlera@allegheny.edu>
2019-04-22 15:04:00 -0400

    Add links for twitter and instagram buttons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,8 +6,8 @@ class Footer extends React.Component {
             <div id="footer">
                 <div className="inner">
                     <ul className="icons">
-                        <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
-                        <li><a href="#" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
+                        <li><a href="https://twitter.com/TubbyBlack" target="_blank" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
+                        <li><a href="https://www.instagram.com/tubbyblack14/" target="_blank" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
                         <p>Social Media</p>
                     </ul>
                     <ul className="copyright">
