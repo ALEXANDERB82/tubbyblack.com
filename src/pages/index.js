f24c9d4321d7615324e6534bb77ab9e05a844a8b
unknown <butlera@allegheny.edu>
2019-05-03 15:09:10 -0400

    Make changes from website reviews

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,11 +22,11 @@ import full06 from '../assets/images/fulls/06.jpg'
 import { Link } from "gatsby"
 
 const DEFAULT_IMAGES = [
-    { id: '1', src: full01, thumbnail: thumb01, caption: 'Love, Me. Vol.2(Prod.Mors)', description: <a href="https://soundcloud.com/tubby-black/love-me-vol-2" target="_blank">Listen Here</a>},
-    { id: '2', src: full02, thumbnail: thumb02, caption: 'Virtuous Tw(o)o(Prod. Beatowski)', description: <a href="https://soundcloud.com/tubby-black/virtuous-tw-o-o" target="_blank">Listen Here</a>},
-    { id: '3', src: full03, thumbnail: thumb03, caption: 'End to End(Prod.Potsu)', description: <a href="https://soundcloud.com/tubby-black/end-to-end" target="_blank">Listen Here</a>},
+    { id: '1', src: full01, thumbnail: thumb01, caption: 'Love, Me. Vol.2 (Prod.Mors)', description: <a href="https://soundcloud.com/tubby-black/love-me-vol-2" target="_blank">Listen Here</a>},
+    { id: '2', src: full02, thumbnail: thumb02, caption: 'Virtuous Tw(o)o (Prod. Beatowski)', description: <a href="https://soundcloud.com/tubby-black/virtuous-tw-o-o" target="_blank">Listen Here</a>},
+    { id: '3', src: full03, thumbnail: thumb03, caption: 'End to End (Prod.Potsu)', description: <a href="https://soundcloud.com/tubby-black/end-to-end" target="_blank">Listen Here</a>},
     { id: '4', src: full04, thumbnail: thumb04, caption: 'Lovely Lives', description: <a href="https://soundcloud.com/tubby-black/lovely-lives" target="_blank">Listen Here</a>},
-    { id: '5', src: full05, thumbnail: thumb05, caption: 'You Feel?(Prod.30HertzBeats)', description: <a href="https://soundcloud.com/tubby-black/you-feel" target="_blank">Listen Here</a>},
+    { id: '5', src: full05, thumbnail: thumb05, caption: 'You Feel? (Prod.30HertzBeats)', description: <a href="https://soundcloud.com/tubby-black/you-feel" target="_blank">Listen Here</a>},
     { id: '6', src: full06, thumbnail: thumb06, caption: 'Never Felt the Love', description: <a href="https://soundcloud.com/tubby-black/never-felt-the-loveprod-illuid-haller" target="_blank">Listen Here</a>}
 ];
 
@@ -94,7 +94,7 @@ class HomeIndex extends React.Component {
                         <header className="major">
                             <h2>Who is Tubby Black?</h2>
                         </header>
-                        <p>I am simply a person who likes to make music. I have benn playing many different instruments from a young age. Honestly my favortie thing about music is writing lyrics. Lyrics give you a chance to speak your mind and let people know what and how you feel, but it also gives you a chance to connect with other people. I am from Pittsburgh, PA and I am gonna make some dope music that will change the world.</p>
+                        <p>I am simply a person who likes to make music. I have been playing many different instruments from a young age. Honestly my favortie thing about music is writing lyrics. Lyrics give you a chance to speak your mind and let people know what and how you feel, but it also gives you a chance to connect with other people. I am from Pittsburgh, PA and I am gonna make some dope music that will change the world.</p>
                         <ul className="actions">
                         </ul>
                     </section>
@@ -116,7 +116,7 @@ class HomeIndex extends React.Component {
 
                     <section id="three">
                         <h2>Hit Me Up</h2>
-                        <p>If you need to get in thouch with me send me an message below.</p>
+                        <p>If you need to get in touch with me send me a message below.</p>
                         <form name="contact" method="post" action="/success" data-netlify="true" data-netlify-honeypot="bot-field">
                         <input type="hidden" name="bot-field" />
                         <input type="hidden" name="form-name" value="contact" />
@@ -141,7 +141,7 @@ class HomeIndex extends React.Component {
                                     </li>
                                     <li>
                                         <h3 className="icon fa-mobile"><span className="label">Phone</span></h3>
-                                        800-053-2357
+                                        412-418-2267
                                     </li>
                                     <li>
                                         <h3 className="icon fa-envelope-o"><span className="label">Email</span></h3>
